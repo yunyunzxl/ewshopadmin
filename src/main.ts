6afa42db5445a18f2d7bb6b8fab2d67469b88271
lmonkey <lmonkey@lmonkey>
2023-02-15 18:40:50 +0800

    抽取路由模块 配置默认端口

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+const app =createApp(App)
+//5.创建并挂在根实例
+app.use(router)
+app.mount('#app')
